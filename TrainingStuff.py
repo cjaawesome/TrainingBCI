cd4724db23621a9c00833db07a8d381e63b898df
cjaawesome <69875824+cjaawesome@users.noreply.github.com>
2023-10-11 17:49:35 -0500

    File Shenanigans

--- TrainingStuff.py
+++ TrainingStuff.py
@@ -1,1 +1,27 @@
-print("This line will be printed.")
+import shutil
+import os
+import pathlib
+from os import path
+
+src_dir = os.listdir(os.getcwd())
+
+tempInd = 0
+temp = []
+for a in src_dir:
+    for b in a:
+        if b == '.':
+            temp.append(a)
+            tempInd = tempInd + 1
+            break
+for a in temp:
+    src_dir.remove(a)
+del temp
+
+print(src_dir)
+
+for a in src_dir:
+    dir = os.getcwd() + '\\' + a
+    print(dir + '\n')
+    print(os.listdir(dir) )
+    print('\n')
+
